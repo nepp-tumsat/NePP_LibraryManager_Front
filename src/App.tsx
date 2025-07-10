b81c12db1cb3292c1e86f0e846b9f6bdcc9d0568
HikaruSuna <sunkun2525@gmail.com>
2025-07-11 00:03:10 +0900

    fix ディレクトリの修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,43 @@ import  API  from './index.tsx'
 import './App.css'
 //import NePPBookContentsUI from './NePPUIPackage';
 import * as NePPUI from './NePPUIPackage';
-import logo from './assets/Lemon!!!.png';
+import { useState } from "react";
 
 
 function App() {
-   const rest = API()
-  console.log(rest)
+   const rest = API();
+  console.log(rest);
+  const [isAvailable , setIsAvailable] = useState(false);
+  const handleClick = () =>{
+       setIsAvailable(prev => !prev)
+  }
+
     return (
         <div style={{
             padding: '40px',
             display: 'flex',
-            gap: '24px',
+            gap: '54px',
             flexWrap: 'wrap' 
         }}>
             <NePPUI.NePPBookContentsUI
-                imageSrc={logo}
-                title="タイトル1"
-                description="説明1"
+                 imageSrc="https://udobiaabiukosfucivbk.supabase.co/storage/v1/object/public/booksimage/World%20Engineer.jpg"
+                 title="世界一流エンジニアの思考法"
+                description="マイクロソフトに入った社員の体験をもとに、一流のエンジニアに必要な要素を学べます"
                 isAvailable={true}
             />
+            <button onClick ={handleClick}>
+                {isAvailable ? "利用可" : "利用中"}
+            </button>
             <NePPUI.NePPBookContentsUI
-                imageSrc={logo}
-                title="タイトル2"
-                description="説明2"
-                isAvailable={false}
-            />
-            <NePPUI.NePPBookContentsUI
-                imageSrc={logo}
-                title="タイトル3"
-                description="説明3"
-                isAvailable={true}
-            />
-            <NePPUI.NePPBookContentsUI
-                imageSrc={logo}
-                title="タイトル4"
-                description="説明4"
-                isAvailable={false}
-            />
+              imageSrc="https://udobiaabiukosfucivbk.supabase.co/storage/v1/object/public/booksimage//picture_large978-4-87311-758-4.jpeg"
+               title="ゼロから作るDeepLearning"
+               description=""
+               isAvailable={false}
+             />
+             <button onClick ={handleClick}>
+                {isAvailable ? "利用可" : "利用中"}
+             </button>
+            
         </div>
     );
 }
