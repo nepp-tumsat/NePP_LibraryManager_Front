aa8b8a839158613cad632590d59eb79723517fc0
HikaruSuna <sunkun2525@gmail.com>
2025-07-11 00:26:21 +0900

    fix 不要なボタンを消去

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import  API  from './index.tsx'
 import './App.css'
 //import NePPBookContentsUI from './NePPUIPackage';
 import * as NePPUI from './NePPUIPackage';
-import { useState } from "react";
 
 
 function App() {
    const rest = API();
   console.log(rest);
-  const [isAvailable , setIsAvailable] = useState(false);
-  const handleClick = () =>{
-       setIsAvailable(prev => !prev)
-  }
+  
+  
 
     return (
         <div style={{
@@ -26,19 +23,13 @@ function App() {
                 description="マイクロソフトに入った社員の体験をもとに、一流のエンジニアに必要な要素を学べます"
                 isAvailable={true}
             />
-            <button onClick ={handleClick}>
-                {isAvailable ? "利用可" : "利用中"}
-            </button>
-            <NePPUI.NePPBookContentsUI
+             <NePPUI.NePPBookContentsUI
               imageSrc="https://udobiaabiukosfucivbk.supabase.co/storage/v1/object/public/booksimage//picture_large978-4-87311-758-4.jpeg"
                title="ゼロから作るDeepLearning"
                description=""
                isAvailable={false}
              />
-             <button onClick ={handleClick}>
-                {isAvailable ? "利用可" : "利用中"}
-             </button>
-            
+
         </div>
     );
 }
