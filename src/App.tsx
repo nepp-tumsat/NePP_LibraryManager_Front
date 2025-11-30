190221e53b866664f74798d06f0cd11c6fade11d
Doragon-RH <r216af@icloud.com>
2025-11-30 17:37:29 +0900

    prettierによるコードの整形

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import { useState,useEffect } from "react";
-import  API  from './func.files/index.tsx'
-import './App.css'
+import { useState, useEffect } from "react";
+import API from "./func.files/index.tsx";
+import "./App.css";
 //import NePPBookContentsUI from './NePPUIPackage';
-import * as NePPUI from './NePPUIPackage';
+import * as NePPUI from "./NePPUIPackage";
 
+function App() {
+  const [books, setBooks] = useState<any[]>([]);
 
-
- function App() {
-  const [books , setBooks] = useState<any[]>([]);
-
-  useEffect(() =>{
-      const fetchData = async () =>{
-        const result = await API();
-        if (result){
-          setBooks(result);
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await API();
+      if (result) {
+        setBooks(result);
       }
-      fetchData();
-  },[])
+    };
+    fetchData();
+  }, []);
 
-    const handleToggle = (id: number) => {
+  const handleToggle = (id: number) => {
     setBooks((prevBooks) =>
       prevBooks.map((book) =>
         book.id === id ? { ...book, isAvailable: !book.isAvailable } : book
@@ -28,32 +26,28 @@ import * as NePPUI from './NePPUIPackage';
   };
   console.log(handleToggle);
 
-
-   return (  
+  return (
     <div
       style={{
-        padding: '20px',
-        display: 'flex',
-        gap: '100px',
-        flexWrap: 'nowrap', 
+        padding: "20px",
+        display: "flex",
+        gap: "100px",
+        flexWrap: "nowrap",
       }}
     >
       {books.map((book) => (
-
-         <div key={book.id} onClick={() => handleToggle(book.id)}>
-        <NePPUI.NePPBookContentsUI
-          key={book.id} 
-          title={book.title}
-          imageSrc={book.cover_image_url} 
-          description={book.description}
-          isAvailable={book.isAvailable}
-        />
+        <div key={book.id} onClick={() => handleToggle(book.id)}>
+          <NePPUI.NePPBookContentsUI
+            key={book.id}
+            title={book.title}
+            imageSrc={book.cover_image_url}
+            description={book.description}
+            isAvailable={book.isAvailable}
+          />
         </div>
-        
       ))}
     </div>
   );
 }
 
-
-export default App
+export default App;
